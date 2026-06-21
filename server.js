9aec8be2b5aaee7b435359529a994785bc6cb6bf
evilbutawesum <jonathanpaulandreoni@gmail.com>
2026-06-21 17:25:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'node:http';
 import { scramjetPath } from '@mercuryworkshop/scramjet/path';
+import { wispServer } from '@titaniumnetwork-dev/wisp-server-node';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -11,13 +12,21 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
-app.use('/scram/', express.static(scramjetPath));
+app.use('/scram', express.static(scramjetPath));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+server.on('upgrade', (req, socket, head) => {
+    if (req.url.startsWith('/wisp/')) {
+        wispServer(req, socket, head);
+    } else {
+        socket.destroy();
+    }
+});
+
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Purify running on port ${PORT}`);
 });

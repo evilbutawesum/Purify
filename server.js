6b983219ecd0e713ab87f430cad86ba235769337
evilbutawesum <jonathanpaulandreoni@gmail.com>
2026-06-21 17:11:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use('/scram', express.static(scramjetPath));
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 

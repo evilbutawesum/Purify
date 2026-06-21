7abb204412d2253b8f89829e8eef880734e939ae
evilbutawesum <jonathanpaulandreoni@gmail.com>
2026-06-21 17:26:35 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
 importScripts("/scram/scramjet.all.js");
+importScripts("/scramjet.config.js");
+
+if (typeof __scramjet$config === "undefined") {
+    self.__scramjet$config = {
+        prefix: '/scram/service/',
+        codec: {
+            encodeUrl: (url) => btoa(url),
+            decodeUrl: (url) => atob(url)
+        },
+        config: '/scramjet.config.js',
+        worker: '/sw.js'
+    };
+}
+
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
-const scramjet = new ScramjetServiceWorker();
+const scramjet = new ScramjetServiceWorker(__scramjet$config);
+
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        scramjet.shouldRoute(event) ? scramjet.handle(event) : fetch(event.request)
-    );
+    if (scramjet.shouldRoute(event)) {
+        event.respondWith(scramjet.handle(event));
+    } else {
+        event.respondWith(fetch(event.request));
+    }
 });
